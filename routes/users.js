56f82dcdeee0c4159237c53f9c07464f1f015625
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-03-09 10:46:03 -0600

    controllers users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const controllers = require("../controllers/users");
 const router = Router();
 
 router.post("/sign-up", controllers.signUp);
-router.post("sign-in", controllesr.signIn);
+router.post("sign-in", controllers.signIn);
 router.get("/verify", controllers.verify);
 router.post("/change-password", controllers.changePassword);
 
