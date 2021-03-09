e124c3e2501234a6a74c2af1163dcb820033f326
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-03-09 10:55:30 -0600

    more errors to fix on cars.js

--- routes/cars.js
+++ routes/cars.js
@@ -5,9 +5,9 @@ const restrict = require("../helpers/restrict");
 const router = Router();
 
 router.get("/", controllers.getCars);
-router.get("/:id".controllers.getCar);
+router.get("/:id", controllers.getCar);
 router.post("/", restrict, controllers.createCar);
-router.put(":id", restrict, constrollers.updateCar);
+router.put(":id", restrict, controllers.updateCar);
 router.delete("/:id", restrict, controllers.deleteCar);
 
 module.exports = router;
