a0904f52429c47ca53f8a1e24cabc910984dd8d6
Andrew Bayer <arbayer@iu.edu>
2021-03-09 11:02:04 -0500

    models complete. Both car and user schema

---
+++ models/car.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Car = new Schema (
+  {
+
+    imgURL: [{ type: String, required: true }], 
+    year: { type: String, required: true },
+    make: { type: String, required: true },
+    model: { type: String, required: true },
+    description: { type: String, required: true },
+    vin: { type: String, required: false },
+    mileage: { type: String, required: false },
+    engine: { type: String, required: false },
+    zipcode: { type: String, required: false },
+    exterior color: { type: String, required: false },
+    interior color: { type: String, required: false },
+    doors: { type: String, required: false },
+    transmission: { type: String, required: true },
+  },
+  {timestamps: true}
+)
+
+module.exports=mongoose.model('cars', Car)
