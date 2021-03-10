17c3fcf50ea4bfefc8c7b24caa8e7c216a533d6a
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-03-10 09:07:30 -0600

    slide show on homepage skeleton

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,11 +1,41 @@
 import React from "react";
-import "./Home.css";
 import Layout from "../../components/shared/Layout/Layout";
+import { Slide } from "react-slideshow-image";
+import "./Home.css";
+// https://www.npmjs.com/package/react-slideshow-image
 
 const Home = (props) => {
+  const slideImages = [
+    "https://i.imgur.com/vmVVnxS.jpeg",
+    "https://imgur.com/nKskSK8",
+    "https://imgur.com/qeTwVVm",
+  ];
+
   return (
     <Layout user={props.user}>
       <div className="home"></div>
+      <div className="slide-container">
+        <Slide>
+          <div className="each-slide">
+            <div style={{ backgroundImage: `url(${slideImages[0]})` }}>
+              <span>Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div
+              className="slide-background"
+              style={{ backgroundImage: `url(${slideImages[1]})` }}
+            >
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{ backgroundImage: `url(${slideImages[2]})` }}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
+      </div>
     </Layout>
   );
 };
