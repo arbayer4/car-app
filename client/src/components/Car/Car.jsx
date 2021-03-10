48572e4a6f1d11a9e3d6db142a0b523493a1198e
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-03-10 15:30:00 -0600

    started css for cars page

--- client/src/components/Car/Car.jsx
+++ client/src/components/Car/Car.jsx
@@ -7,19 +7,21 @@ const Car = (props) => {
     <>
       <Link className="car" to={`/cars/${props._id}`}>
         <img className="car-image" src={props.imgURL} alt={props.make} />
-        <div className="car-year">{props.year}</div>
-        <div className="car-make">{props.make}</div>
-        <div className="car-model">{props.model}</div>
-        <div className="car-description">{props.description}</div>
-        <div className="car-price">{props.price}</div>
-        <div className="car-vin">{props.vin}</div>
-        <div className="car-mileage">{props.mileage}</div>
-        <div className="car-engine">{props.engine}</div>
-        <div className="car-zipcode">{props.zipcode}</div>
-        <div className="car-exterior-color">{props.exteriorColor}</div>
-        <div className="car-interior-color">{props.interiorColor}</div>
-        <div className="car-doors">{props.doors}</div>
-        <div className="car-transmission">{props.transmission}</div>
+        <div className="car-details-fp">
+          <div className="car-year">{props.year}</div>
+          <div className="car-make">{props.make}</div>
+          <div className="car-model">{props.model}</div>
+          <div className="car-description">{props.description}</div>
+          <div className="car-price">${props.price}</div>
+          <div className="car-vin">{props.vin}</div>
+          <div className="car-mileage">{props.mileage}</div>
+          <div className="car-engine">{props.engine}</div>
+          <div className="car-zipcode">{props.zipcode}</div>
+          <div className="car-exterior-color">{props.exteriorColor}</div>
+          <div className="car-interior-color">{props.interiorColor}</div>
+          <div className="car-doors">{props.doors}</div>
+          <div className="car-transmission">{props.transmission}</div>
+        </div>
       </Link>
     </>
   );
