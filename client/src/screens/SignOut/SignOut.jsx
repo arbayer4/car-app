705def39252c1efb1bdb3531a084dfa88106e0f6
Andrew Bayer <arbayer@iu.edu>
2021-03-10 08:01:41 -0500

    sign-out added

--- client/src/screens/SignOut/SignOut.jsx
+++ client/src/screens/SignOut/SignOut.jsx
@@ -1,1 +1,18 @@
+import React, { useEffect } from "react";
+import { useHistory } from "react-router";
+import { signOut } from "../../services/users";
 
+const SignOut = (props) => {
+  const { clearUser, user } = props;
+  const history = useHistory();
+
+  useEffect(() => {
+    signOut(user)
+      .then(() => clearUser())
+      .finally(() => history.push("/"));
+  }, [history, clearUser, user]);
+
+  return "";
+};
+
+export default SignOut;
