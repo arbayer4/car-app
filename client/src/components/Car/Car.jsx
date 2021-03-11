6aa4bb3fbaaeaa5c968dfb99f5ebcf83b2ff51c0
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-03-11 13:29:46 -0600

    merging

--- client/src/components/Car/Car.jsx
+++ client/src/components/Car/Car.jsx
@@ -5,20 +5,22 @@ import { Link } from "react-router-dom";
 const Car = (props) => {
   return (
     <Link className="car" to={`/cars/${props._id}`}>
-          <div className="container-all">
-            <div className="container-image-and-data">
-              <div className="container-image">
-                <img className="car-image" src={props.imgURL} alt={props.make} />
-              </div>
-              <div className="container-data">
-                <div className="car-title">{props.year} {props.make} {props.model}</div>
-                <div className="car-description">{props.description}</div>
-                <div className="car-price">${props.price}</div>
-              </div>
-            </div>
-            <div className="container-orange-card"></div>
+      <div className="container-all">
+        <div className="container-image-and-data">
+          <div className="container-image">
+            <img className="car-image" src={props.imgURL} alt={props.make} />
           </div>
-        </Link>
+          <div className="container-data">
+            <div className="car-title">
+              {props.year} {props.make} {props.model}
+            </div>
+            <div className="car-description">{props.description}</div>
+            <div className="car-price">${props.price}</div>
+          </div>
+        </div>
+        <div className="container-orange-card"></div>
+      </div>
+    </Link>
   );
 };
 
