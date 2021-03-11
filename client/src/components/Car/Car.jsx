c539d201946a16ca9b1b6e7db22e6567ed36180e
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-03-11 09:50:11 -0600

    cars css not fixed

--- client/src/components/Car/Car.jsx
+++ client/src/components/Car/Car.jsx
@@ -8,22 +8,27 @@ const Car = (props) => {
       <Link className="car" to={`/cars/${props._id}`}>
         <div className="image-container">
           <img className="car-image" src={props.imgURL} alt={props.make} />
-        </div>
-        <div className="car-details-fp">
-          <div className="car-year">{props.year}</div>
-          <div className="car-make">{props.make}</div>
-          <div className="car-model">{props.model}</div>
-          <div className="car-description">{props.description}</div>
-          <div className="car-price">${props.price}</div>
-          <div className="car-vin">{props.vin}</div>
-          <div className="car-mileage">{props.mileage}</div>
-          <div className="car-engine">{props.engine}</div>
-          <div className="car-zipcode">{props.zipcode}</div>
-          <div className="car-exterior-color">{props.exteriorColor}</div>
-          <div className="car-interior-color">{props.interiorColor}</div>
-          <div className="car-doors">{props.doors}</div>
-          <div className="car-transmission">{props.transmission}</div>
-          <Link to={`/cars/${props._id}`}>Veiw More</Link>
+          <div className="car-details-fp">
+            <div className="hidden"></div>
+            <div className="details-fp">
+              <div className="car-year">{props.year}</div>
+              <div className="car-make">{props.make}</div>
+              <div className="car-model">{props.model}</div>
+              <div className="car-description">{props.description}</div>
+              <div className="car-price">${props.price}</div>
+              <div className="car-vin">{props.vin}</div>
+              <div className="car-mileage">{props.mileage}</div>
+              <div className="car-engine">{props.engine}</div>
+              <div className="car-zipcode">{props.zipcode}</div>
+              <div className="car-exterior-color">{props.exteriorColor}</div>
+              <div className="car-interior-color">{props.interiorColor}</div>
+              <div className="car-doors">{props.doors}</div>
+              <div className="car-transmission">{props.transmission}</div>
+              <Link className="veiw-more" to={`/cars/${props._id}`}>
+                Veiw More
+              </Link>
+            </div>
+          </div>
         </div>
       </Link>
     </>
