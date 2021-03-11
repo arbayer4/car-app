e2b94dd27846b67b1291674cc77a1c5b477ea761
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-03-11 16:50:05 -0600

    fixed cars screen

--- client/src/components/Car/Car.jsx
+++ client/src/components/Car/Car.jsx
@@ -4,23 +4,26 @@ import { Link } from "react-router-dom";
 
 const Car = (props) => {
   return (
-    <Link className="car" to={`/cars/${props._id}`}>
-      <div className="container-all">
-        <div className="container-image-and-data">
-          <div className="container-image">
+    <div className="container-all">
+      <div className="container-image-and-data">
+        <div className="container-image">
+          <Link className="car" to={`/cars/${props._id}`}>
             <img className="car-image" src={props.imgURL} alt={props.make} />
-          </div>
-          <div className="container-data">
-            <div className="car-title">
-              {props.year} {props.make} {props.model}
-            </div>
-            <div className="car-description">{props.description}</div>
-            <div className="car-price">${props.price}</div>
-          </div>
+          </Link>
         </div>
-        <div className="container-orange-card"></div>
+        <div className="container-data">
+          <div className="car-title">
+            {props.year} {props.make} {props.model}
+          </div>
+          <div className="car-description">{props.description}</div>
+          <p className="car-price">${props.price}</p>
+          <Link className="veiw-more-detail" to={`/cars/${props._id}`}>
+            Veiw More
+          </Link>
+        </div>
       </div>
-    </Link>
+      <div className="container-orange-card"></div>
+    </div>
   );
 };
 
