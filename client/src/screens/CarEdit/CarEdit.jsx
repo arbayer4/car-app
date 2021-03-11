c0ecae40184e326796a93c584ffb4e52909c9b32
Daniel Ober <ChillDrummer@MacBook-Pro.local>
2021-03-11 00:04:18 -0600

    some css enhancements to edit page and details page

--- client/src/screens/CarEdit/CarEdit.jsx
+++ client/src/screens/CarEdit/CarEdit.jsx
@@ -24,7 +24,6 @@ const CarEdit = (props) => {
 
   const [isUpdated, setUpdated] = useState(false);
   let { id } = useParams();
-  //   console.log(id);
 
   useEffect(() => {
     const fetchCar = async () => {
@@ -44,7 +43,6 @@ const CarEdit = (props) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // let { id } = props.match.params;
     const updated = await updateCar(id, car);
     setUpdated({ updated });
   };
@@ -58,112 +56,136 @@ const CarEdit = (props) => {
       <form className="edit-form" onSubmit={handleSubmit}>
         <div className="top">
           <div className="left-details">
-            <label htmlFor="make">Make:</label>
-            <input
-              type="text"
-              name="make"
-              id="make"
-              value={car.make}
-              required
-              autoFocus
-              onChange={handleChange}
-            />
-            <label htmlFor="model">Model:</label>
-            <input
-              type="text"
-              name="model"
-              id="model"
-              value={car.model}
-              required
-              onChange={handleChange}
-            />{" "}
-            <label htmlFor="year">Year:</label>
-            <input
-              type="text"
-              name="year"
-              id="year"
-              value={car.year}
-              required
-              onChange={handleChange}
-            />{" "}
-            <label htmlFor="zipcode">Zip Code:</label>
-            <input
-              type="text"
-              name="zipcode"
-              id="zipcode"
-              value={car.zipcode}
-              onChange={handleChange}
-            />{" "}
-            <label htmlFor="exterior-color">Exterior Color</label>
-            <input
-              type="text"
-              name="exteriorColor"
-              id="exterior-color"
-              value={car.exteriorColor}
-              onChange={handleChange}
-            />{" "}
-            <label htmlFor="doors">Doors:</label>
-            <input
-              type="text"
-              name="doors"
-              id="doors"
-              value={car.doors}
-              onChange={handleChange}
-            />
+            <div className="label-input">
+              <label htmlFor="make">Make:</label>
+              <input
+                type="text"
+                name="make"
+                id="make"
+                value={car.make}
+                required
+                autoFocus
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="model">Model:</label>
+              <input
+                type="text"
+                name="model"
+                id="model"
+                value={car.model}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="year">Year:</label>
+              <input
+                type="text"
+                name="year"
+                id="year"
+                value={car.year}
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="zipcode">Zip Code:</label>
+              <input
+                type="text"
+                name="zipcode"
+                id="zipcode"
+                value={car.zipcode}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="exterior-color">Exterior Color:</label>
+              <input
+                type="text"
+                name="exteriorColor"
+                id="exterior-color"
+                value={car.exteriorColor}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="doors">Doors:</label>
+              <input
+                type="text"
+                name="doors"
+                id="doors"
+                value={car.doors}
+                onChange={handleChange}
+              />
+            </div>
           </div>
           <div className="right-details">
-            <label htmlFor="mileage">Mileage:</label>
-            <input
-              type="text"
-              name="mileage"
-              id="mileage"
-              value={car.mileage}
-              onChange={handleChange}
-            />
-            <label htmlFor="transmission">Transmission:</label>
-            <input
-              type="text"
-              name="transmission"
-              id="transmission"
-              value={car.transmission}
-              onChange={handleChange}
-            />{" "}
-            <label htmlFor="engine">Engine:</label>
-            <input
-              type="text"
-              name="engine"
-              id="engine"
-              value={car.engine}
-              onChange={handleChange}
-            />{" "}
-            <label htmlFor="vin">VIN:</label>
-            <input
-              type="text"
-              name="vin"
-              id="vin"
-              value={car.vin}
-              onChange={handleChange}
-            />{" "}
-            <label htmlFor="interior-color">Interior Color:</label>
-            <input
-              type="text"
-              name="interiorColor"
-              id="interior-color"
-              value={car.interiorColor}
-              onChange={handleChange}
-            />
-            <label htmlFor="price">Price:</label>
-            <input
-              type="text"
-              name="price"
-              id="price"
-              value={car.price}
-              onChange={handleChange}
-            />
+            <div className="label-input">
+              <label htmlFor="mileage">Mileage:</label>
+              <input
+                type="text"
+                name="mileage"
+                id="mileage"
+                value={car.mileage}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="transmission">Transmission:</label>
+              <input
+                type="text"
+                name="transmission"
+                id="transmission"
+                value={car.transmission}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="engine">Engine:</label>
+              <input
+                type="text"
+                name="engine"
+                id="engine"
+                value={car.engine}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="vin">VIN:</label>
+              <input
+                type="text"
+                name="vin"
+                id="vin"
+                value={car.vin}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="interior-color">Interior Color:</label>
+              <input
+                type="text"
+                name="interiorColor"
+                id="interior-color"
+                value={car.interiorColor}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="label-input">
+              <label htmlFor="price">Price:</label>
+              <input
+                type="text"
+                name="price"
+                id="price"
+                value={car.price}
+                onChange={handleChange}
+              />
+            </div>
           </div>
         </div>
         <div className="bottom-details">
-          <label htmlFor="description">Description</label>
+          <label htmlFor="description">Description:</label>
           <textarea
             name="description"
             id="description"
@@ -173,7 +195,7 @@ const CarEdit = (props) => {
             value={car.description}
             onChange={handleChange}
           />
-          <label htmlFor="images">Images</label>
+          <label htmlFor="images">Images:</label>
           <input
             type="url"
             name="imgURL"
@@ -182,10 +204,10 @@ const CarEdit = (props) => {
             required
             onChange={handleChange}
           />
-        </div>
         <button className="save-button" type="submit">
-          Save
+          Save Changes
         </button>
+        </div>
       </form>
     </Layout>
   );
