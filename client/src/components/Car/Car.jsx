b766089c6a22cd39658dde6f8b992e8c416d533e
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-03-12 12:56:23 -0600

    updates to cars. again

--- client/src/components/Car/Car.jsx
+++ client/src/components/Car/Car.jsx
@@ -6,20 +6,22 @@ const Car = (props) => {
   return (
     <div className="container-all">
       <div className="container-image-and-data">
-        <div className="container-image">
+        <div className="container-image-fp">
           <Link className="car" to={`/cars/${props._id}`}>
-            <img className="car-image" src={props.imgURL} alt={props.make} />
+            <img className="car-image-fp" src={props.imgURL} alt={props.make} />
           </Link>
         </div>
         <div className="container-data-fp">
-          <div className="car-title">
+          <div className="car-title-fp">
             {props.year} {props.make} {props.model}
           </div>
-          <div className="car-description">{props.description}</div>
-          <p className="car-price">${props.price}</p>
-          <Link className="veiw-more-detail" to={`/cars/${props._id}`}>
-            View More
-          </Link>
+          <div className="car-description-fp">{props.description}</div>
+          <div className="price-and-viewmore">
+            <div className="car-price-fp">${props.price}</div>
+            <Link className="view-more-detail" to={`/cars/${props._id}`}>
+              View More
+            </Link>
+          </div>
         </div>
       </div>
       <div className="container-orange-card"></div>
