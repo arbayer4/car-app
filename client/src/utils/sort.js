4a5f9587ec8858c9cccc2bde2da3e0da83140966
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-03-12 11:04:20 -0600

    cars orange box back!

--- client/src/utils/sort.js
+++ client/src/utils/sort.js
@@ -1,10 +1,10 @@
-const compareKey = (key) => (a, b) => {
-  a[key] < b[key] == b[key] ? (a[key] < b[key] ? -1 : 1) : 0;
-};
+// const compareKey = (key) => (a, b) => {
+//   a[key] < b[key] == b[key] ? (a[key] < b[key] ? -1 : 1) : 0;
+// };
 
-export const AZ = (arr) => arr.sort(compareKey("make"));
-export const ZA = (arr) => arr.sort(compareKey("make")).reverse();
-export const lowestFirst = (arr) =>
-  arr.sort((a, b) => parseInt(a.price) - parseInt(b.price));
-export const highestFirst = (arr) =>
-  arr.sort((a, b) => parseInt(b.price) - parseInt(a.price));
+// export const AZ = (arr) => arr.sort(compareKey("make"));
+// export const ZA = (arr) => arr.sort(compareKey("make")).reverse();
+// export const lowestFirst = (arr) =>
+//   arr.sort((a, b) => parseInt(a.price) - parseInt(b.price));
+// export const highestFirst = (arr) =>
+//   arr.sort((a, b) => parseInt(b.price) - parseInt(a.price));
