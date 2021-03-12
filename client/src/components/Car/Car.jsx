985d11757100e833d5b0a4271784ba58159e6a73
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-03-12 15:17:06 -0600

    adding commas to price

--- client/src/components/Car/Car.jsx
+++ client/src/components/Car/Car.jsx
@@ -3,26 +3,21 @@ import "./Car.css";
 import { Link } from "react-router-dom";
 
 const Car = (props) => {
-  let newPrice = props.price.split(" ");
-  let num = newPrice.length;
-  console.log(newPrice);
-  switch (num) {
-    case num >= 4:
-      newPrice.splice(3, 0, ",");
-      break;
-    case num >= 9:
-      newPrice.splice(3, 0, ",");
-      newPrice.splice();
-    default:
-      break;
-  }
-
+  let newPrice = props.price.replace(/\d(?=(?:\d{3})+$)/g, "$&,"); //recieved from stackoverflow https://stackoverflow.com/questions/27311714/adding-commas-to-numbers-when-typing
   return (
     <div className="container-all">
       <div className="container-image-and-data">
         <div className="container-image-fp">
           <Link className="car" to={`/cars/${props._id}`}>
-            <img className="car-image-fp" src={props.imgURL ? props.imgURL : "https://images.unsplash.com/photo-1606017116209-b1ed168465e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80"} alt={props.make} />
+            <img
+              className="car-image-fp"
+              src={
+                props.imgURL
+                  ? props.imgURL
+                  : "https://images.unsplash.com/photo-1606017116209-b1ed168465e8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80"
+              }
+              alt={props.make}
+            />
           </Link>
         </div>
         <div className="container-data-fp">
