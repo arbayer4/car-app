7e3c4835daf5352f02a0f9ed759dae1033de41c7
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-03-15 13:11:24 -0500

    nav

--- client/src/components/shared/Nav/Burger.jsx
+++ client/src/components/shared/Nav/Burger.jsx
@@ -9,25 +9,25 @@ const StyledBurger = styled.div`
   right: 20px;
   display: flex;
   justify-content: space-around;
-  flex-flow: column no wrap;
+  flex-flow: column nowrap;
 
   div {
     width: 2rem;
     height: 0.25rem;
-    background-color: ${({ open }) => (open ? "#ccc" : "#333")};
+    background-color: ${({ open }) => (open ? "#ffffff" : "white")};
     border-radius: 10px;
     transform-origin: 1px;
     transition: all 0.3s linear;
 
-    &:nth=child(1) {
-      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0deg)")};
+    &:nth-child(1) {
+      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
     }
-    &:nth=child(2) {
+    &:nth-child(2) {
       transform: ${({ open }) => (open ? "translateX(100%)" : "translateX(0)")};
       opacity: ${({ open }) => (open ? 0 : 1)};
     }
-    &:nth=child(3) {
-      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0deg)")};
+    &:nth-child(3) {
+      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
     }
   }
 `;
